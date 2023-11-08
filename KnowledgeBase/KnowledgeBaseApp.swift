d58068bb06993c0d4b38ae44d1b9a5cb4ae35b13
Evgeniya Shevyakova <stativa.zhe@gmail.com>
2023-11-08 12:08:36 +0300

    Initial Commit

---
+++ KnowledgeBase/KnowledgeBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowledgeBaseApp.swift
+//  KnowledgeBase
+//
+//  Created by Евгения Шевякова on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct KnowledgeBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
